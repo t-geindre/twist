25c3030f7c03c075e25a966c50b89d097c8cacc9
Thierry Geindre <github.com@nostalgeek.org>
2019-03-05 11:47:44 +0100

    Browser stabilization

--- src/Twitter/Browser/Client.php
+++ src/Twitter/Browser/Client.php
@@ -2,16 +2,17 @@
 
 namespace Twist\Twitter\Browser;
 
-use Twist\Twitter\Browser\Exception\BadCredentialsException;
-use Twist\Twitter\Browser\Exception\NotLoggedInException;
 use HeadlessChromium\Browser;
+use HeadlessChromium\Exception\JavascriptException;
+use HeadlessChromium\Exception\OperationTimedOut;
 use HeadlessChromium\Page;
+use HeadlessChromium\PageUtils\PageEvaluation;
 use Psr\Log\LoggerInterface;
 
 class Client
 {
     const LOGIN_URL = 'https://twitter.com/login';
-    const SEARCH_URL = 'https://twitter.com/search-home';
+    const IDLE_URL = 'https://twitter.com/search-home';
 
     /** @var Page */
     private $page;
@@ -28,47 +29,51 @@ class Client
     /** @var int */
     private $requestsCount = 0;
 
+    /** @var Browser */
+    private $browser;
+
+    /** @var string */
+    private $headersInterceptorScript;
+
+    /** @var string */
+    private $requestProxyScript;
+
     public function __construct(
         Browser $browser,
         LoggerInterface $logger,
         string $headersInterceptorScript,
         string $requestProxyScript
     ) {
-        $this->page = $browser->createPage();
-
-        $this->page->addPreScript(
-            file_get_contents($headersInterceptorScript)."\n".
-            file_get_contents($requestProxyScript)
-        );
         $this->logger = $logger;
+        $this->browser = $browser;
+        $this->headersInterceptorScript = $headersInterceptorScript;
+        $this->requestProxyScript = $requestProxyScript;
+
+        $this->createPage();
     }
 
     public function login(string $username, string $password): bool
     {
         $this->logger->info('Logging in Twitter');
 
-        $this->page->navigate(self::LOGIN_URL);
-        $this->page->waitForReload();
-
-        if (strpos($this->page->getCurrentUrl(), self::LOGIN_URL) === false) {
-            $this->fetchRequestHeaders();
-
-            return $this->loggedIn = true;
-        }
-
-        $this->page->evaluate('
-            $("form.signin input[type=text].email-input").val('.json_encode($username).');
-            $("form.signin input[type=password]").val('.json_encode($password).');
-            $("form.signin").submit();
-        ');
-
-        $this->page->waitForReload(Page::DOM_CONTENT_LOADED);
+        $this->page->navigate(self::LOGIN_URL)->waitForNavigation();
 
         if (strpos($this->page->getCurrentUrl(), self::LOGIN_URL) !== false) {
-            throw new BadCredentialsException('Login failed, check username and password');
+            $this->evaluate('
+                $("form.signin input[type=text].email-input").val('.json_encode($username).');
+                $("form.signin input[type=password]").val('.json_encode($password).');
+                $("form.signin").submit();
+            ');
+
+            $this->page->waitForReload(Page::DOM_CONTENT_LOADED);
+
+            if (strpos($this->page->getCurrentUrl(), self::LOGIN_URL) !== false) {
+                throw new \RuntimeException('Login failed, check username and password');
+            }
         }
 
         $this->fetchRequestHeaders();
+        $this->reloadPage();
 
         return $this->loggedIn = true;
     }
@@ -80,29 +85,19 @@ class Client
         // Reload page to avoid overload
         if ($this->requestsCount++ > 300) {
             $this->requestsCount = 0;
-            $this->page->evaluate('window.document.location.reload()');
-            $this->page->waitForReload();
+            $this->reloadPage();
         }
 
-        $settings = array_merge_recursive(
-            $settings,
-            [
-                'headers' => array_merge(
-                    $this->getRequestHeaders(),
-                    ['x-csrf-token' => $this->getCsrfToken()]
-                ),
-                'xhrFields' => ['withCredentials' => true]
-            ]
-        );
+        $settings = array_merge_recursive($settings, [
+            'headers' => array_merge($this->getRequestHeaders(), ['x-csrf-token' => $this->getCsrfToken()]),
+            'xhrFields' => ['withCredentials' => true]
+        ]);
 
         $uid = uniqid();
-        $this->page->evaluate('twist.sendRequest(
-            '.json_encode($uid).',
-            '.json_encode($settings).'
-        )');
+        $this->evaluate('twist.sendRequest('.json_encode($uid).', '.json_encode($settings).')');
 
         for(;;) {
-            $result = $this->page->evaluate('twist.getRequestResult('.json_encode($uid).')')->getReturnValue();
+            $result = $this->evaluate('twist.getRequestResult('.json_encode($uid).')')->getReturnValue();
             if ($result['status'] === 'pending') {
                 usleep(100000); // 100ms
                 continue;
@@ -121,6 +116,19 @@ class Client
         return $result['data'];
     }
 
+    public function evaluate(string $script, bool $handleException = true): PageEvaluation
+    {
+        try {
+            return $this->page->evaluate($script);
+        } catch (OperationTimedOut $e) {
+            if ($handleException) {
+                $this->reloadPage();
+                return $this->evaluate($script, false);
+            }
+            throw $e;
+        }
+    }
+
     public function getRequestHeaders(): ?array
     {
         $this->assertLoggedIn();
@@ -130,7 +138,13 @@ class Client
 
     public function getCsrfToken(): ?string
     {
-        return $this->page->evaluate('(/ct0=([^;]+)/).exec(document.cookie)[1]')->getReturnValue();
+        $this->assertLoggedIn();
+
+        try {
+            return $this->evaluate('(/ct0=([^;]+)/).exec(document.cookie)[1]')->getReturnValue();
+        } catch (JavascriptException $e) {
+            throw new \RuntimeException('Unable to retriew CSRF Token', 0, $e);
+        }
     }
 
     protected function fetchRequestHeaders(): void
@@ -143,22 +157,38 @@ class Client
 
         do {
             // Scroll down until there's an API call
-            $this->page->evaluate("window.scrollTo(0,document.body.scrollHeight);");
+            $this->evaluate("window.scrollTo(0,document.body.scrollHeight);");
             usleep(100000); // 100ms
-            $this->requestHeaders = $this->page->evaluate('twist.getInterceptedHeaders()')->getReturnValue();
+            $this->requestHeaders = $this->evaluate('twist.getInterceptedHeaders()')->getReturnValue();
         } while (false === $this->requestHeaders);
+    }
 
-        // Navigate to search page, avoid overload
-        $this->page->navigate(self::SEARCH_URL);
-        $this->page->waitForReload();
+    protected function reloadPage()
+    {
+        $url = null;
+        if (null !== $this->page) {
+            $url = $this->page->getCurrentUrl();
+            $this->page->close();
+        }
+
+        $this->createPage();
+        $this->page->navigate($url ? $url : self::IDLE_URL)->waitForNavigation();
+    }
+
+    protected function createPage(): void
+    {
+        $this->page = $this->browser->createPage();
+
+        $this->page->addPreScript(
+            file_get_contents($this->headersInterceptorScript)."\n".
+            file_get_contents($this->requestProxyScript)
+        );
     }
 
     protected function assertLoggedIn(): void
     {
         if (!$this->loggedIn) {
-            throw new NotLoggedInException('Login is required before making any other browser call');
+            throw new \RuntimeException('Unabe to perform action: not logged in');
         }
     }
 }
-
-
