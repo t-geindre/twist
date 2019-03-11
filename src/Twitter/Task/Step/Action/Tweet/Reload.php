728f5c96dc1f56ccb8802399f84134c8a969dedc
Thierry Geindre <github.com@nostalgeek.org>
2019-03-11 15:20:04 +0100

    Handle deleted statuses

--- src/Twitter/Task/Step/Action/Tweet/Reload.php
+++ src/Twitter/Task/Step/Action/Tweet/Reload.php
@@ -3,6 +3,7 @@
 namespace Twist\Twitter\Task\Step\Action\Tweet;
 
 use Twist\Twitter\Api\Client;
+use Twist\Twitter\Browser\Exception\RequestException;
 use Twist\Twitter\Task\Step\Action\ActionInterface;
 use Twist\Twitter\Task\ConfigurableInterface;
 
@@ -21,10 +22,22 @@ class Reload implements ActionInterface, ConfigurableInterface
 
     public function execute(array $tweet): ?array
     {
-        return $this->client->getStatus(array_merge(
-            ['id' => $tweet['id_str']],
-            $this->config
-        ));
+        $status = null;
+
+        try {
+            $status = $this->client->getStatus(array_merge(
+                ['id' => $tweet['id_str']],
+                $this->config
+            ));
+        } catch (RequestException $e) {
+            if ($e->getCode() != 404) {
+                throw $e;
+            }
+            // Tweet might have been deleted
+            // Especially if search API is the source (it's cached)
+        }
+
+        return $status;
     }
 
     public function configure(array $config): void
